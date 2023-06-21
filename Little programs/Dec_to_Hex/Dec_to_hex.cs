81cef6cba99df021b0d03a4589fb04b4c653cef5
Abdulloh Halilov <109743592+UZBeKHalilov@users.noreply.github.com>
2023-06-21 15:42:34 +0500

    Added Little programs
    here my programs for work myself. age calculator and Dec to hex not made by me!. i changed sth

---
+++ Little programs/Dec_to_Hex/Dec_to_hex.cs
@@ -0,0 +1,29 @@
+﻿namespace homework03
+{
+    internal class Dec_to_Hex
+    {
+        static void Main(string[] args)
+        {
+            int number, i;
+            int[] array = new int[10];
+
+            Console.Write("\n\tEnter the number to convert: ");
+            number = int.Parse(Console.ReadLine());
+
+            for (i = 0; number > 0; i++)
+            {
+                array[i] = number % 2;
+                number = number / 2;
+            }
+
+            Console.Write("\n\tBinary of the given number => ");
+
+            for (i = i - 1; i >= 0; i--)
+            {
+                Console.Write(array[i]);
+            }
+            
+            Console.WriteLine();
+        }
+    }
+}
